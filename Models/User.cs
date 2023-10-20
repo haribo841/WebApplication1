bfd888fdd51c00534c7b5115e49745cdda81fdaf
haribo841 <adamwaldemarkubis@gmail.com>
2023-10-20 23:40:20 +0200

    Add project files.

---
+++ Models/User.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public required string Name { get; set; }
+        [Required]
+        public required string Surname { get; set; }
+        [Required]
+        public required bool Gender { get; set; }
+        [Required]
+        public required DateTime BirthDate { get; set; }
+        public int TelephoneNumber { get; set; }
+        public string? Position { get; set; }
+        public int ShoeSize { get; set; }
+        public User()
+        {
+                
+        }
+    }
+}
